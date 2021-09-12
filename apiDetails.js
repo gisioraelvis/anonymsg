7be89b452888705a5a167ef9200daadcacb62256
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-09-12 11:31:00 +0300

    REST API Endpoints

---
+++ apiDetails.js
@@ -0,0 +1,43 @@
+//An Object containing details about routes available in this API
+
+export const apiDetails = {
+  message: "anonymsg REST API",
+  endpoints: [
+    {
+      url: "/user/login",
+      params: ["username", "password"],
+      description: "Allow the user to login by generating a jwt",
+    },
+    {
+      url: "/user/logout",
+      params: [],
+      description: "Allow the user to logout from the device",
+    },
+    {
+      url: "/user/register",
+      params: ["name", "username", "password"],
+      description: "Allow new user to register for the service",
+    },
+    {
+      url: "/user/dashboard",
+      params: [],
+      description:
+        "Returns Username,Name and Messages of Logged In User (User should be logged in to have access)",
+    },
+    {
+      url: "/user/password",
+      params: ["username", "password", "newPassword"],
+      description: "Change password of existing user",
+    },
+    {
+      url: "/user/:username",
+      params: [],
+      description: "Get details of the specific user",
+    },
+    {
+      url: "/message/:username",
+      params: ["message"],
+      description: "Sends message to the corresponding username",
+    },
+  ],
+};
